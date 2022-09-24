3888d4aab1b50aeab72b22dd7a9e22d4b6d1cbeb
os-bigdata <113830516+os-bigdata@users.noreply.github.com>
2022-09-24 07:09:24 +0530

    Update ConfusionMatrix.py

--- ConfusionMatrix.py
+++ ConfusionMatrix.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-d= pd.read_csv(r"C:\Users\sonas\Downloads\Social_Network_Ads (1).csv")
+d= pd.read_csv(r"C:\Users\sonas\Downloads\Social_Network_Ads.csv")
 x= d.iloc[:,:-1]
 y = d.iloc[:,-1]
 
